49e17eba2798cad501847dd601f6006eb5b92a00
Zulqernain mahar <ah1845773@gmail.com>
2026-05-16 02:42:23 +0500

    Typecast rootElement as HTMLElement

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
     <App />
